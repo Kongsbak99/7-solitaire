42e90678e37410e7c0450142ef5d012b408cb996
Krzysztof Kisiel <71651955+ExoHumann@users.noreply.github.com>
2022-03-02 13:03:35 +0100

    Redcolor

--- ImageRecognition/image.py
+++ ImageRecognition/image.py
@@ -1,16 +1,47 @@
 import cv2 as cv
-import time
-
+import numpy as np
 cap = cv.VideoCapture(0)
+while(1):
+    # Take each frame
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # define range of red color in HSV
+    # Threshold the HSV image to get only blue colors
+    # Bitwise-AND mask and original image
+
+    #lower red
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+
+    #upper red
+    lower_red2 = np.array([170,50,50])
+    upper_red2 = np.array([180,255,255])
+
+    mask = cv.inRange(hsv, lower_red, upper_red)
+    res = cv.bitwise_and(frame,frame, mask= mask)
+
+    mask2 = cv.inRange(hsv, lower_red2, upper_red2)
+    res2 = cv.bitwise_and(frame,frame, mask= mask2)
+
+    #Black mask
+    lower_black = np.array([0,0,0])
+    upper_black = np.array([100,100,100])
+
+    mask3 = cv.inRange(hsv, lower_black, upper_black)
+    res3 = cv.bitwise_and(frame, frame, mask = mask3)
+
+    
+    img4 = cv.add(mask,mask2,mask3)
+    
 
 
-#Reading the image from capture
-img = cv.imread(cap)
+    
+    cv.imshow('Original',frame)
+    cv.imshow('res4',img4)
 
- 
-cv.imwrite('data_{}.jpg'.format(time.time()), img)
-print("Saving Image")
-
- 
-cap.relese()
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
 cv.destroyAllWindows()
