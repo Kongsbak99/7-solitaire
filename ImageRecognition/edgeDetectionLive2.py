0cdc7f86aebae979017010c05a19cfff7ec1016d
Marcus <mkottosen@gmail.com>
2022-06-02 17:31:01 +0200

    Made some corrections to the orientation of the card

--- ImageRecognition/edgeDetectionLive2.py
+++ ImageRecognition/edgeDetectionLive2.py
@@ -1,4 +1,6 @@
 import math
+import sys
+
 import numpy as np
 import cv2
 import numpy.linalg as la
@@ -39,6 +41,44 @@ class GetCardCorner:
             x2 = x2 + x1
             return x2
 
+        def correctOrientation(n):  # Sets first coordinates to be top-left corner.
+            # OBS: Can still be oriented wrong even if first coordinates is top-left corner
+            try:
+                ##corrected = [None] * 8  # empty array at fixed size
+                #corrected = np.array(n)
+                min_x = sys.maxsize
+                min_y = sys.maxsize
+                move_amount = 0
+
+                # Find corner with lowest number (lowest should be top left corner)
+                for u in range(0, len(n), 2):
+                    #print("==== is " + str(n[u]) + " less than " + str(min_x))
+                    print("--- " + str(u) + " to " + str(u+1))
+                    print("--- " + str(n[u]) + ", " + str(n[u+1]) + " = " + str(n[u] + n[u+1]) + "\n")
+                    #if n[u] < min_x and n[u+1] < min_y:
+                    if n[u] + n[u+1] < min_x + min_y:
+                        min_x = n[u]
+                        min_y = n[u+1]
+                print("============ min-x and min-y: " + str(min_x) + ", " + str(min_y))
+
+                # Figure out how many indexes to shift array so corner with the lowest value is top-left
+                if min_x in n:
+                    move_amount = np.where(n == min_x)
+                    move_amount = int(move_amount[0])
+                    print("indexes to move: " + str(move_amount))
+
+                # Finally shift array left
+                if move_amount != 0:
+                    corrected = np.roll(n, -move_amount)
+                    print("finished rotating")
+
+                    for q in range(0, len(n), 2):
+                        print(str(corrected[q]) + ", " + str(corrected[q+1]))
+                    return corrected
+                return n
+            except:
+                print("Error in correctOrientation()")
+                return n
 
         # Going through every contours found in the image.
         for cnt in contours:
@@ -56,9 +96,9 @@ class GetCardCorner:
                     if linelength > minLineSize:
                         cv2.drawContours(img, [approx], 0, (0, 0, 255), 2)
                         # print("i: " + str(i))
-                        print("coordinates: " + str([approx]))  # + ", " + (n[i + 2])
+                        print("coordinates: " + "\n" + str([approx]))  # + ", " + (n[i + 2])
                     else:
-                        break
+                        break  # breaks a lot
                 except:
                     i = i  # Cannot be empty..
 
@@ -73,7 +113,7 @@ class GetCardCorner:
                         except:
                             string = str(x) + " " + str(y)
 
-                        pts1 = np.float32([[n[0], n[1]], [n[2], n[3]], [n[4], n[5]], [n[6], n[7]]])
+                        cardCoordinates = np.float32([[n[0], n[1]], [n[2], n[3]], [n[4], n[5]], [n[6], n[7]]])
                         # ptsCut = np.float32([[0, 0], [0, 450], [300, 450], [300, 0]])
                         ptsCut = np.float32([[0, 0], [0, 450], [300, 450], [300, 0]])
 
@@ -87,19 +127,20 @@ class GetCardCorner:
                         rad = np.arccos(np.clip(cos, -1.0, 1.0))
                         deg = np.rad2deg(rad)
 
+                        n = correctOrientation(n)
+
                         # only if degrees are reasonable, create and show warp & place text on original image
                         if 80 < deg < 100:
                             height = round(math.sqrt(((n[0] - n[2]) ** 2) + ((n[1] - n[3]) ** 2)), 2)
                             width = round(math.sqrt(((n[6] - n[0]) ** 2) + ((n[7] - n[1]) ** 2)), 2)
-                            print(height)
-                            print(width)
-                            print("----")
+                            #print(height)
+                            #print(width)
 
+                            # print("is: " + str(height) + " 1.5 times bigger than: " + str(width) + " = " + str(
+                            #   width * 1.5))
                             # If cards are stacked, do some math to find only the top card.
-                            print("is: " + str(height) + " 1.5 times bigger than: " + str(width) + " = " + str(
-                                width * 1.5))
                             if height > width * 1.5:
-                                pts1 = np.float32([
+                                cardCoordinates = np.float32([
                                     [shorten(n[2], n[0], height, width), shorten(n[3], n[1], height, width)],
                                     [n[2], n[3]],
                                     # [shorten(n[0], n[2], height, width), shorten(n[1], n[3], height, width)],
@@ -115,30 +156,27 @@ class GetCardCorner:
                                 # print(str(shorten(n[0], n[2], height, width)) + ", " + str(shorten(n[1], n[3], height, width)))
                                 # print(str(shorten(n[6], n[4], height, width)) + ", " + str(shorten(n[7], n[5], height, width)))
                                 # print(str(n[6]) + ", " + str(n[7]))
-                            print(pts1)
+                            print(cardCoordinates)
 
                             # [((n[2]) / height) * (width * 1.5), (n[3] / height) * (width * 1.5)],
                             # [(n[4] / height) * (width * 1.5), (n[5] / height) * (width * 1.5)],
 
-                            M = cv2.getPerspectiveTransform(pts1, ptsCut)
+                            # cardCoordinates = coordinates of the cards corners in image
+                            # ptsCut = Size of card we want after warp
+                            M = cv2.getPerspectiveTransform(cardCoordinates, ptsCut)
+                            # Applies the warp on the image
                             dst = cv2.warpPerspective(imgGray, M, (300, 450))
 
                             # Get the bottom left corner
-                            # botPts = np.float32([
-                            #   [n[2], (n[3]-width/2.2)],
-                            #   [n[2], n[3]],
-                            #   [(n[2]+width/5), n[3]],
-                            #   [(n[2]+width/5), (n[3]-width/2.2)]])
-                            # Get the bottom left corner with better math
-                            botPts = np.float32([
+                            CornerCutCoordinates = np.float32([
                                 [botShortenY(n[2], n[0], height, width), botShortenY(n[3], n[1], height, width)],
                                 [n[2], n[3]],
                                 [botShortenX(n[2], n[4], height, width), botShortenX(n[3], n[5], height, width)],
                                 [botShortenX(n[2], n[6], height, width), botShortenY(n[3], n[7], height, width)]])
 
-                            botCut = np.float32([[0, 0], [0, 130], [120, 130], [120, 0]]) #120 x 240
-                            botM = cv2.getPerspectiveTransform(botPts, botCut)
-                            botDst = cv2.warpPerspective(imgGray, botM, (120, 130))
+                            botCut = np.float32([[0, 0], [0, 240], [120, 240], [120, 0]])  # the coordinates/size of the new image
+                            botM = cv2.getPerspectiveTransform(CornerCutCoordinates, botCut)
+                            botDst = cv2.warpPerspective(imgGray, botM, (120, 240))
                             cv2.imshow("card zoom", botDst)
 
                             # Remove noise from the image. THIS USES A LOT OF PROCESSING POWER
@@ -172,3 +210,13 @@ class GetCardCorner:
         cv2.imshow('image2', img)
     cv2.destroyAllWindows()
     pass
+
+#
+#   n index:
+#
+#   0,1     6,7
+#
+#
+#
+#   2,3     4,5
+#
--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -1,7 +1,7 @@
 from ImageRecognition.edgeDetectionLive2 import GetCardCorner
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while(1):
     _, frame = cap.read()
@@ -9,7 +9,17 @@ while(1):
     cardCorner.GetCardCorner(frame)
 
 
-    
+
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+
+# import numpy as np
+# first_array = [1, 2, 3, 4, 5, 6]
+# my_array = np.array(first_array)
+#
+# new_array = np.roll(my_array, 2)  # rotate right
+# print(new_array)
+#
+# new_array = np.roll(my_array, -2)  # rotate left
+# print(new_array)
