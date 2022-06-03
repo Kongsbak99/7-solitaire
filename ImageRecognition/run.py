ef7bd72be50e3efc919d969dfacd89a125b180f9
marcu <dsf@asd.com>
2022-06-03 15:52:49 +0200

    Video stream to 1280 x 720p

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -2,6 +2,8 @@ from edgeDetectionLive2 import GetCardCorner
 import cv2
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while(1):
     _, frame = cap.read()
@@ -11,19 +13,3 @@ while(1):
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-
-# import cv2
-# video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#
-# cv2.namedWindow("Window")
-#
-# while True:
-#     ret, frame = video_capture.read()
-#     cv2.imshow("Window", frame)
-#
-#     #This breaks on 'q' key
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# video_capture.release()
-# cv2.destroyAllWindows()
