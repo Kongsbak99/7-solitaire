47b6dc83834f8755fed4b1cc9e9eafe0dd4aac95
Krzysztof Kisiel <qminec@gmail.com>
2022-06-03 20:31:07 +0200

    fixed error

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -5,19 +5,18 @@ import cv2
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
+count = 0
 while (1):
     _, frame = cap.read()
     cardCorner = GetCardCorner()
+
     try:
         cardCornerPicture = cardCorner.GetCardCorner(frame)
-        bw_filter = bw_filter(cardCorner.GetCardCorner(frame))
+        bw_filter = bw_filter(cardCornerPicture)
     except:
         print("Could not find card corner")
 
-    key = cv2.waitKey(5)
-    count = 0
-
+    key = cv2.waitKey(5)  # Scan the keystrokes
     if key == ord('q'):  # Wait for q key to capture a frame.
         cv2.imwrite("TrainingImages/frame%d.jpg" % count, cardCornerPicture)  # save frame as JPEG file
         cv2.imshow('CardCornerPicture', cardCornerPicture)  # Display the resulting frame
