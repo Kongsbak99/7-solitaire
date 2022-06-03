bd07f28f1b4d064e0ba8a829705b093edf0a05d2
Krzysztof Kisiel <qminec@gmail.com>
2022-06-03 19:16:33 +0200

    Saving images as JPEG

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -4,13 +4,13 @@ import cv2
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+count = 0
 
 while (1):
     _, frame = cap.read()
     cardCorner = GetCardCorner()
     CardCornerPicture = cardCorner.GetCardCorner(frame)
 
-    count = 0
     key = cv2.waitKey(33) & 0b11111111  # Wait for q key to capture a frame.
     k = cv2.waitKey(5) & 0xFF  # Wait for key ESC to break
 
