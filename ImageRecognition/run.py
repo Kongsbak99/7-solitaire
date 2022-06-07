2ab36beb59715058b6ad47b55942b1458b7e537e
Marcus <mkottosen@gmail.com>
2022-06-07 10:31:39 +0200

    ImageSplit works with edgeDetection now

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -2,27 +2,30 @@ from ImageRecognition.edgeDetectionLive2 import GetCardCorner
 import cv2
 from ImageRecognition.imageSplit import navn
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
-while(1):
+while 1:
     _, frame = cap.read()
-    navn(cap)
-    cardCorner = GetCardCorner()
-    cardCorner.GetCardCorner(frame)
+    listOfFrames = navn(frame)
+    # navn(frame)
+    # cardCorner = GetCardCorner()
 
-
-
+    cv2.imshow('preview', frame)
+    # cv2.imshow(1, listOfFrames[1])
+    # for i in range(0, len(listOfFrames)):
+    #     #cv2.imshow(str(i), listOfFrames[i])
+    #     cardCorner = GetCardCorner(listOfFrames[i])
+    #     cv2.imshow((str(i), frame))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        box1 = GetCardCorner(listOfFrames[7])
+        box2 = GetCardCorner(listOfFrames[8])
+        box3 = GetCardCorner(listOfFrames[9])
+        box4 = GetCardCorner(listOfFrames[10])
+        cv2.imshow("num1", box1)
+        cv2.imshow("num2", box2)
+        cv2.imshow("num3", box3)
+        cv2.imshow("num4", box4)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-
-# import numpy as np
-# first_array = [1, 2, 3, 4, 5, 6]
-# my_array = np.array(first_array)
-#
-# new_array = np.roll(my_array, 2)  # rotate right
-# print(new_array)
-#
-# new_array = np.roll(my_array, -2)  # rotate left
-# print(new_array)
