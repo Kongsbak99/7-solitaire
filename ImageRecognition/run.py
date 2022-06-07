fc4ea1dc5032708d86ce6bc1c9b354e0d852f9c7
Marcus <mkottosen@gmail.com>
2022-06-07 10:47:07 +0200

    Merge & saves images at buttonpress

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -1,4 +1,3 @@
-from ImageRecognition.old.image import bw_filter
 from edgeDetectionLive2 import GetCardCorner
 import cv2
 from ImageRecognition.imageSplit import navn
@@ -8,11 +7,9 @@ cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 count = 0
-while (1):
+while 1:
     _, frame = cap.read()
     listOfFrames = navn(frame)
-    # navn(frame)
-    # cardCorner = GetCardCorner()
 
     cv2.imshow('preview', frame)
     # cv2.imshow(1, listOfFrames[1])
@@ -20,46 +17,29 @@ while (1):
     #     #cv2.imshow(str(i), listOfFrames[i])
     #     cardCorner = GetCardCorner(listOfFrames[i])
     #     cv2.imshow((str(i), frame))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('r'):
         box1 = GetCardCorner(listOfFrames[7])
         box2 = GetCardCorner(listOfFrames[8])
         box3 = GetCardCorner(listOfFrames[9])
         box4 = GetCardCorner(listOfFrames[10])
-        cv2.imshow("num1", box1)
+        #cv2.imshow("num1", box1)
         cv2.imshow("num2", box2)
         cv2.imshow("num3", box3)
         cv2.imshow("num4", box4)
 
+        try:
+            #cardCornerPicture = GetCardCorner(frame)
+            #bw_filter = bw_filter(box1)
 
-    try:
-        cardCornerPicture = cardCorner.GetCardCorner(frame)
-        bw_filter = bw_filter(cardCornerPicture)
-    except:
-        print("Could not find card corner")
+            cv2.imwrite("TrainingImages/frame%d.jpg" % count, box2)  # save frame as JPEG file
+            cv2.imshow('CardCornerPicture', box2)  # Display the resulting frame
 
-    key = cv2.waitKey(5)  # Scan the keystrokes
-    if key == ord('q'):  # Wait for q key to capture a frame.
-        cv2.imwrite("TrainingImages/frame%d.jpg" % count, cardCornerPicture)  # save frame as JPEG file
-        cv2.imshow('CardCornerPicture', cardCornerPicture)  # Display the resulting frame
-        count += 1
-        continue
-    elif key == 27:  # Wait for key ESC to break
+            count += 1
+                #continue
+        except:
+            print("Could not find card corner")
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         cap.release()
         break
-
-# import cv2
-# video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#
-# cv2.namedWindow("Window")
-#
-# while True:
-#     ret, frame = video_capture.read()
-#     cv2.imshow("Window", frame)
-#
-#     #This breaks on 'q' key
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# video_capture.release()
-# cv2.destroyAllWindows()
