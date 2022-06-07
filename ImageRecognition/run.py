c219210f5612a496081661c1f3adacd4bfd96e17
Marcus <mkottosen@gmail.com>
2022-06-07 11:36:14 +0200

    Text field on each field in preview window

--- ImageRecognition/run.py
+++ ImageRecognition/run.py
@@ -1,3 +1,4 @@
+from ImageRecognition.write_on_image import write_on_image
 from edgeDetectionLive2 import GetCardCorner
 import cv2
 from ImageRecognition.imageSplit import navn
@@ -11,31 +12,28 @@ while 1:
     _, frame = cap.read()
     listOfFrames = navn(frame)
 
+    for i in range(0, 12):
+        write_on_image(frame, i, count)
+
     cv2.imshow('preview', frame)
-    # cv2.imshow(1, listOfFrames[1])
-    # for i in range(0, len(listOfFrames)):
-    #     #cv2.imshow(str(i), listOfFrames[i])
-    #     cardCorner = GetCardCorner(listOfFrames[i])
-    #     cv2.imshow((str(i), frame))
+
     if cv2.waitKey(1) & 0xFF == ord('r'):
-        box1 = GetCardCorner(listOfFrames[7])
-        box2 = GetCardCorner(listOfFrames[8])
-        box3 = GetCardCorner(listOfFrames[9])
-        box4 = GetCardCorner(listOfFrames[10])
-        #cv2.imshow("num1", box1)
-        cv2.imshow("num2", box2)
-        cv2.imshow("num3", box3)
-        cv2.imshow("num4", box4)
+        box1 = GetCardCorner(listOfFrames[8])
+        box2 = GetCardCorner(listOfFrames[9])
+        box3 = GetCardCorner(listOfFrames[10])
+        cv2.imshow("num0", box1)
+        cv2.imshow("num1", box2)
+        cv2.imshow("num2", box3)
 
         try:
-            #cardCornerPicture = GetCardCorner(frame)
-            #bw_filter = bw_filter(box1)
+            # cardCornerPicture = GetCardCorner(frame)
+            # bw_filter = bw_filter(box1)
 
             cv2.imwrite("TrainingImages/frame%d.jpg" % count, box2)  # save frame as JPEG file
             cv2.imshow('CardCornerPicture', box2)  # Display the resulting frame
 
             count += 1
-                #continue
+            # continue
         except:
             print("Could not find card corner")
 
