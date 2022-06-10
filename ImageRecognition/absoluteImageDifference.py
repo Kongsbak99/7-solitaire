9e183f7db230d1537e4598bc948f44a916f1534f
Gfauser <gufa1353@gmail.com>
2022-06-10 10:19:46 +0200

    BestCardMatch can iterate through cards and find best match

--- ImageRecognition/absoluteImageDifference.py
+++ ImageRecognition/absoluteImageDifference.py
@@ -7,8 +7,8 @@ from cv2 import absdiff
 path = os.path.dirname(os.path.abspath(__file__))
 pathname= path + '/ValidationImages/'
 imageformat = ".jpg"
-knight = cv2.imread('knight3.jpg')
-trainimage1 = cv2.imread(pathname+"h4.jpg")
+knight = cv2.imread(pathname+'da.jpg')
+
 
 class PlaceholderCards:
     def __init__(self):
@@ -26,9 +26,9 @@ def loadTrainingCards():
         validation_cards.append(PlaceholderCards())
         validation_cards[i].name = Cards
         imageName = validation_cards[i].name + imageformat
-        validation_cards[i].image = pathname + imageName
-        imageFileLocation = validation_cards[i].image
-        #print(validation_cards[i].image)
+        validation_cards[i].image = cv2.imread(pathname + imageName)
+
+
         i = i + 1
 
     return validation_cards
@@ -41,19 +41,24 @@ def loadTrainingCards():
 
 def bestCardMatch(image):
 
-    baseDiff = 10000
+    baseDiff = 20000
 
-    #for Cards in loadTrainingCards():
-        #diff_img = cv2.absdiff(image,Cards.image)
-        #rank_diff = int(np.sum(diff_img) / 255)
-       # if rank_diff < baseDiff:
-         #   baseDiff = rank_diff
+    for Cards in loadTrainingCards():
+        diff_img = cv2.absdiff(image,Cards.image)
+        rank_diff = int(np.sum(diff_img) / 255)
+
+        print(Cards.name,rank_diff)
+
+        if rank_diff < baseDiff:
+            baseDiff = rank_diff
+            bestCardName = Cards.name
         #cv2.imread(Cards.image)
         #cv2.imshow("absc",Cards.image)
         #print(Cards.image)
-        #cv2.imread(Cards.image)
-       # cv2.imread(Cards.image)
-        #print(rank_diff)
+
+            #print(Cards.name)
+
+    print("Best matching card is:",bestCardName, "the value is:",baseDiff)
 
         #cv2.imshow("h",Cards.image)
 
@@ -76,17 +81,21 @@ def showCardsDiff():
     diffTest2 = absdiff(knight, heart10)
     valueCalc1 = int(np.sum(diffTest1) / 255)
     valueCalc2 = int(np.sum(diffTest2) / 255)
-    print("Difference between knight & knight2 is:",valueCalc1)
-    print("Difference between knight & heart10 is:",valueCalc2)
+    print("Difference between knight & knight2 is:", valueCalc1)
+    print("Difference between knight & heart10 is:", valueCalc2)
 
-while 1:
-    cv2.imshow("abc", trainimage1)
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-
-        bestCardMatch(trainimage1)
-        #loadTrainingCards()
-       # cv2.imshow("abc",trainimage1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+bestCardMatch(knight)
+# while 1:
+#     cv2.imshow("a",knight)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('r'):
+#         #diff_img = cv2.absdiff(trainimage1,trainimage2)
+#         #rank_diff = int(np.sum(diff_img) / 255)
+#         #a = 10
+#
+#         #loadTrainingCards()
+#        # cv2.imshow("abc",trainimage1)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         cv2.destroyAllWindows()
+#         break
 
