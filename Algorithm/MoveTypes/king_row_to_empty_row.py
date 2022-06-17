0db7700b8b3659944d98ebe19c287df67e8ead0b
Kongsbak99 <victoreskongsbak@gmail.com>
2022-06-17 14:58:04 +0200

    merge

--- Algorithm/MoveTypes/king_row_to_empty_row.py
+++ Algorithm/MoveTypes/king_row_to_empty_row.py
@@ -4,41 +4,40 @@
 
 def kingRowToEmptyRow(move, board):
     row = board["row-stack"][move["to"]]
-    card = move["cards"][0]
     cards = move["cards"]
 
     cards_reversed = list(reversed(cards))
 
     # Find original row of card
-    if card in board["row-stack"]["row-1"]:
+    if cards[0] in board["row-stack"]["row-1"]:
         original_row = "row-1"
-    elif card in board["row-stack"]["row-2"]:
+    elif cards[0] in board["row-stack"]["row-2"]:
         original_row = "row-2"
-    elif card in board["row-stack"]["row-3"]:
+    elif cards[0] in board["row-stack"]["row-3"]:
         original_row = "row-3"
-    elif card in board["row-stack"]["row-4"]:
+    elif cards[0] in board["row-stack"]["row-4"]:
         original_row = "row-4"
-    elif card in board["row-stack"]["row-5"]:
+    elif cards[0] in board["row-stack"]["row-5"]:
         original_row = "row-5"
-    elif card in board["row-stack"]["row-6"]:
+    elif cards[0] in board["row-stack"]["row-6"]:
         original_row = "row-6"
-    elif card in board["row-stack"]["row-7"]:
+    elif cards[0] in board["row-stack"]["row-7"]:
         original_row = "row-7"
     else:
-        print("ERROR - Cannot find row for card with ID: " + str(card))
+        print("ERROR - Cannot find row for card with ID: " + str(cards[0]))
 
     # Remove the cards from the orignal row
     for card in cards_reversed:
         board["row-stack"][original_row].remove(card)
-
-    # Add the card to the empty row
-    for card in cards_reversed:
-        row.insert(0, card)
 
     # Make top leftover card visible if not
     # But only if there are cards left
     if board["row-stack"][original_row]:
         if board["row-stack"][original_row][0] == 0:
             board["row-stack"][original_row][0] = -1
+
+    # Add the card to the empty row
+    for card in cards_reversed:
+        row.insert(0, card)
 
     return board
